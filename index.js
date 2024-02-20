e64c62db3fd68942a19251d4db90a189924807f4
saurav kunwor <kunworsaurav3@gmail.com>
2024-02-20 18:13:54 +0545

    first commit

---
+++ index.js
@@ -0,0 +1,61 @@
+const timeDisplay = document.querySelector("#timeDisplay");
+const starBtn = document.querySelector("#startBtn");
+const pauseBtn = document.querySelector("#pauseBtn");
+const resetBtn = document.querySelector("#resetBtn");
+
+let starTime = 0;
+let elapsedTime = 0;
+let currentTime = 0;
+let paused = true;
+let intervalid;
+let hrs = 0;
+let mins = 0;
+let secs = 0;
+
+starBtn.addEventListener("click", () => {
+    if (paused) {
+        paused = false;
+        starTime = Date.now() - elapsedTime;
+        intervalid = setInterval(updateTime, 75);
+
+    }
+});
+pauseBtn.addEventListener("click", () => {
+    if (!paused) {
+        paused = true;
+        elapsedTime = Date.now() - starTime;
+        clearInterval(intervalid);
+    }
+});
+resetBtn.addEventListener("click", () => {
+    paused = true;
+    clearInterval(intervalid);
+    starTime = 0;
+    elapsedTime = 0;
+    currentTime = 0;
+    hrs = 0;
+    mins = 0;
+    secs = 0;
+    timeDisplay.textContent = "00.00.00";
+
+});
+
+function updateTime() {
+    elapsedTime = Date.now() - starTime;
+
+    secs = Math.floor((elapsedTime / 1000) % 60);
+    mins = Math.floor((elapsedTime / (1000 * 60)) % 60);
+    hrs = Math.floor((elapsedTime / (1000 * 60 * 60)) % 60);
+
+
+
+    secs = pad(secs);
+    mins = pad(mins);
+    hrs = pad(hrs);
+
+    timeDisplay.textContent = `${hrs}:${mins}:${secs}`;
+
+    function pad(unit) {
+        return (("0") + unit).length > 2 ? unit : "0" + unit;
+    }
+}
